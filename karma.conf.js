8ec9b33bee9de0ad31b5e5b476ce54aa8fce780c
rishabhdixit <rishabh.dixit@intelligrape.com>
2016-03-08 16:09:15 +0530

    Added karma test folder

--- karma.conf.js
+++ karma.conf.js
@@ -1,68 +1,88 @@
 // Karma configuration
-// Generated on Tue Mar 08 2016 14:23:14 GMT+0530 (IST)
+// Generated on Wed Sep 02 2015 14:15:39 GMT+0530 (IST)
 
 module.exports = function(config) {
-  config.set({
+    config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
 
 
-    // list of files / patterns to load in the browser
-    files: [
-    ],
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['jasmine'],
 
 
-    // list of files to exclude
-    exclude: [
-    ],
+        // list of files / patterns to load in the browser
+        files: [
+            'test/assets/bower_components/jquery/dist/jquery.min.js',
+            'test/assets/bower_components/angular/angular.js',
+            'test/assets/bower_components/angular-animate/angular-animate.min.js',
+            'test/assets/bower_components/angular-route/angular-route.min.js',
+            'test/assets/bower_components/angular-bootstrap/ui-bootstrap.min.js',
+            'test/assets/bower_components/owlcarousel/owl-carousel/owl.carousel.min.js',
+            'test/assets/bower_components/tinymce/tinymce.min.js',
+            'test/assets/bower_components/angular-mocks/angular-mocks.js',
+            'test/assets/*.js',
+            'control/content/**/*.js',
+            'control/design/**/*.js',
+            'control/settings/**/*.js',
+            'widget/**/*.js',
+            'test/**/*.spec.js'
+        ],
+
+        // list of files to exclude
+        exclude: [
+        ],
 
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+            'control/**/!(assets|js)/*.js':['coverage'],
+            'widget/*.js':['coverage'],
+            'widget/!(assets|js)/*.js':['coverage']
+        },
+        plugins: [
+            'karma-phantomjs-launcher',
+            'karma-jasmine',
+            'karma-junit-reporter',
+            'karma-coverage'
+        ],
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['progress', 'coverage'],
+
+        coverageReporter: {
+            type: 'html',
+            dir: 'test/coverage/'
+        },
+
+        // web server port
+        port: 9876,
 
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
 
-    // web server port
-    port: 9876,
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
 
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['PhantomJS'],
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
-  })
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: false
+    })
 }
---
+++ test/control/design/app.spec.js
@@ -0,0 +1,8 @@
+describe('wooCommercePlugin Design: app', function () {
+
+    describe('Buildfire service', function () {
+        it('Buildfire should exists', function () {
+            expect(true).toBeDefined();
+        });
+    });
+});
